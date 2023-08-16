4ba3ded3f8c243f4b30acc92bc63547b08173f3f
naotaka nakane <n.nakane0219@gmail.com>
2023-08-16 19:29:36 +0900

    Fix variable names.

--- install.sh
+++ install.sh
@@ -5,21 +5,22 @@ set -euo pipefail
 DOTFILES_DIR="${HOME}/dotfiles"
 GITHUB_USER_NAME="gtnao0219"
 GITHUB_REPOSITORY_NAME="dotfiles-new"
-GIT_HUB_URL="git@github.com:${GITHUB_USER_NAME}/${GITHUB_REPOSITORY_NAME}.git"
-TARBALL="https://github.com/${GITHUB_USER_NAME}/${GITHUB_REPOSITORY_NAME}/archive/master.tar.gz"
+BRANCH_NAME="main"
+GITHUB_URL="git@github.com:${GITHUB_USER_NAME}/${GITHUB_REPOSITORY_NAME}.git"
+TARBALL="https://github.com/${GITHUB_USER_NAME}/${GITHUB_REPOSITORY_NAME}/archive/${BRANCH_NAME}.tar.gz"
 
 if [ ! -d "${DOTFILES_DIR}" ]; then
   if command -v git >/dev/null 2>&1; then
-    git clone "${GIT_HUB_URL}" "${DOTFILES_DIR}"
+    git clone "${GITHUB_URL}" "${DOTFILES_DIR}"
   elif command -v curl >/dev/null 2>&1 || command -v wget >/dev/null 2>&1; then
     if command -v curl >/dev/null 2>&1; then
-      curl -L "${TARBALL}" -o master.tar.gz
+      curl -L "${TARBALL}" -o "${BRANCH_NAME}.tar.gz"
     else
       wget "${TARBALL}"
     fi
-    tar -zxvf master.tar.gz
-    rm -f master.tar.gz
-    mv "$GITHUB_REPOSITORY_NAME}-master" "${DOTFILES_DIR}"
+    tar -zxvf "${BRANCH_NAME}.tar.gz"
+    rm -f "${BRANCH_NAME}.tar.gz"
+    mv "${GITHUB_REPOSITORY_NAME}-${BRANCH_NAME}" "${DOTFILES_DIR}"
   else
     echo "git, curl, wget is not found"
     exit 1
