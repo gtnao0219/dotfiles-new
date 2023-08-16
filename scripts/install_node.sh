fad6f1ef212bc04ebdff546bd6ac8abd3be3e325
naotaka nakane <n.nakane0219@gmail.com>
2023-08-16 22:34:31 +0900

    comment out purge nodejs.

--- scripts/install_node.sh
+++ scripts/install_node.sh
@@ -5,7 +5,7 @@ set -eu
 VERSION=18
 
 sudo apt update
-sudo apt purge -y nodejs
+# sudo apt purge -y nodejs
 curl -fsSL "https://deb.nodesource.com/setup_${VERSION}.x" | sudo -E sh -
 sudo apt install -y nodejs
 
